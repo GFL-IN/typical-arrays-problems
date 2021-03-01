0e90fcaa8457302e1ea3ed879b1dca74a0fe617f
Gleb Korotkevich <gena.gfl.fgl@gmail.com>
2021-03-01 20:24:01 +0300

    docs: add index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  }
+  else {
+    let min_i = array[0];
+    array.forEach(item => {
+      if (item < min_i ) min_i = item;
+    });
+
+    return min_i;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  }
+  else {
+    let max_i = array[0];
+    array.forEach(item => {
+      if (item > max_i ) max_i = item;
+    });
+
+    return max_i;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (typeof array === 'undefined' || array.length === 0) {
+    return 0;
+  }
+  else {
+    let sum = 0;
+    let count = 0;
+    array.forEach(item => {
+      sum += item;
+      count += 1;
+    });
+
+    return sum / count;
+  }
 }
